3df8d85a7c95a60ce098394e140c731cdbb1f477
Alexey Bondarenko <alexeybondarenko@me.com>
2015-12-30 15:52:05 +0200

    update objects

--- src/js/controllers/main-controller.js
+++ src/js/controllers/main-controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('app').controller('MainController', function ($scope) {
+angular.module('app').controller('MainController', function ($scope, ApplicationCollection) {
 
   $scope.objects = [];
   $scope.update = function () {
-
+    $scope.objects = ApplicationCollection.loadApplications();
   };
 
   $scope.$on('$destroy', function () {
--- src/js/controllers/main-controller_test.js
+++ src/js/controllers/main-controller_test.js
@@ -24,7 +24,15 @@ describe('MainController', function () {
     it ('should success destroy', function () {
       scope.$destroy();
       expect(true).toEqual(true);
-    })
-  })
+    });
+
+    it ('should update objects from ApplicationCollection.loadApplications', inject(function (_ApplicationCollection_) {
+      spyOn(_ApplicationCollection_,'loadApplications').and.returnValue([1,2,3]);
+      scope.update();
+      expect(scope.objects).toEqual([1,2,3]);
+    }))
+
+  });
+
 
 });
